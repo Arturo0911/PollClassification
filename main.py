2167b165daceadc23d9ce680898812632fa8a3d8
Payload <anegreiross@outlook.com>
2021-09-21 00:23:31 -0500

    added new features

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 
 import numpy as np
 import pandas as pd
+import glob
+import itertools
+from PIL import Image
+from torch.utils.data import (
+    Dataset,
+    DataLoader
+)
+
+from utils.utils import ReplayBuffer
+
+from torchvision.transforms import transforms
+
+
+class ImageDataset(Dataset):
+    def __init__(self, base_dir, transform=None, split="train"):
+        self.transform = transforms.Compose(transform)
+        self.file_A = glob.glob(os.path.join(base_dir, '{}/A/*.*'.format(split)))
+
 
 
 def main():
