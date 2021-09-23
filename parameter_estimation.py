67ac61f3bd4ecf63c51713e2e40bcb45a319d343
Payload <anegreiross@outlook.com>
2021-09-23 02:32:10 -0500

    ok

--- parameter_estimation.py
+++ parameter_estimation.py
@@ -79,8 +79,32 @@ def principal_function_chapter5():
 
 
 def main():
-    
 
+    t_c = [0.5,  14.0, 15.0, 28.0, 11.0,  8.0,  3.0, -4.0,  6.0, 13.0, 21.0]
+    t_u = [35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4]
+    t_c = torch.tensor(t_c).unsqueeze(1) # <1>
+    t_u = torch.tensor(t_u).unsqueeze(1) # <1>
+
+    t_u.shape
+
+    n_samples = t_u.shape[0]
+    n_val = int(0.2 * n_samples)
+
+    shuffled_indices = torch.randperm(n_samples)
+
+    train_indices = shuffled_indices[:-n_val]
+    val_indices = shuffled_indices[-n_val:]
+
+    train_indices, val_indices
+    
+    t_u_train = t_u[train_indices]
+    t_c_train = t_c[train_indices]
+
+    t_u_val = t_u[val_indices]
+    t_c_val = t_c[val_indices]
+
+    t_un_train = 0.1 * t_u_train
+    t_un_val = 0.1 * t_u_val
     def training_loop(n_epochs, optimizer, model, 
         loss_fn, t_u_train, t_u_val, t_c_train, t_c_val):
 
@@ -98,13 +122,26 @@ def main():
 
 
             if epoch == 1 or epoch % 1000 == 0:
-                print(f"Epoch {epoch}, Training loss {loss_train.item():.4f},
-                e"f" Validation loss {loss_val.item():.4f}")
-
+                print(f"Epoch {epoch}, Training loss {loss_train.item():.4f},"
+                    f" Validation loss {loss_val.item():.4f}")
 
 
     linear_model = nn.Linear(1, 1)
     optimizer = torch.optim.SGD(linear_model.parameters(), lr=1e-2)
+
+
+    training_loop(n_epochs = 3000,
+            optimizer = optimizer,
+            model = linear_model,
+            loss_fn = nn.MSELoss(),
+            t_u_train = t_un_train,
+            t_u_val = t_un_val,
+            t_c_train = t_c_train,
+            t_c_val = t_c_val)
+    
+
+    print(linear_model.weight)
+    print(linear_model.bias)
 
 if __name__ == "__main__":
     # gradient = GradientDescent()
--- scratch.py
+++ scratch.py
@@ -2,14 +2,27 @@
 
 import torch
 from torch import nn
-
+from torchvision import datasets
+from torch.nn import functional as F
 
 
 def main():
-    x = torch.ones(1)
-    linear_model = nn.Linear(1, 1)
-    print(linear_model(x))
+    x = torch.randn(3, 5)
+    y = torch.ones_like(x)
 
+    cifar = datasets.CIFAR10(".datasets", download=True)
+    data = torch.Tensor(cifar.data)
+    print(data.size())
+
+
+    class Net(nn.Module):
+        def __init__(self, num_channels: int):
+            super(Net, self).__init__()
+            self.num_channels = num_channels
+
+            self.conv1 = 
+            self.conv2 = 
+            self.conv3 = 
 
 if __name__ == "__main__":
     main()
