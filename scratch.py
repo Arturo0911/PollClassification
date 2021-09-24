bf6c4dc57a94e0d5ba795804978934e306c26f18
Payload <anegreiross@outlook.com>
2021-09-23 19:05:29 -0500

    training ok

--- scratch.py
+++ scratch.py
@@ -5,13 +5,15 @@ from typing import Any
 import torch
 from torch import nn
 from torchvision import datasets
+from torchvision import transforms
 from torch.nn import functional as F
 from torch.utils.data import (
     Dataset,
     DataLoader
 )
+from torchvision.utils import make_grid
 from PIL import Image
-
+from torch import optim
 
 
 class Net(nn.Module):
@@ -58,10 +60,9 @@ class Net(nn.Module):
         x = F.relu(x)
 
         x = self.fc2(x)
-        x = F.softmax(x, dim=1)
+        x = F.log_softmax(x, dim=1)
 
         return x
-
 
 
 # mouting dataset
@@ -85,16 +86,65 @@ class SIGNSDataset(Dataset):
         return image, self.targets[idx]
 
 
-def main():
-    # x = torch.randn(3, 5)
-    # y = torch.ones_like(x)
 
-    # cifar = datasets.CIFAR10(".datasets", download=True)
-    # data = torch.Tensor(cifar.data)
-    # print(data.size())
-    signs = SIGNSDataset(base_dir="datasets", split="train")
-    print(len(signs))
-    print(signs[0][0])
+class RunningMetric:
+    def __init__(self):
+        self.S = 0
+        self.N = 0
+    
+    def update(self, val, size):
+        self.S += val
+        self.N += size
+    
+    def __call__(self):
+        return self.S/float(self.N)
+
+
+
+def main():
+    signs = SIGNSDataset(base_dir="datasets", 
+                        split="train", 
+                        transform=transforms.ToTensor())
+
+    # deliver in batches to the neuron network
+    dataloader = DataLoader(signs, batch_size=32)
+
+    network = Net(32)
+    loss_function = nn.NLLLoss()
+    optimizer = optim.SGD(network.parameters(), lr=1e-3, momentum=0.9)
+
+
+    epochs = 100
+    for epoch in range(epochs):
+        print(f"Epoch => {epoch}/{epochs}")
+
+        running_loss = RunningMetric() # error ice in the network
+        running_acc = RunningMetric() # ice of the accuracy
+
+        for inputs, targets in dataloader:
+            # reload gradients to zero
+            # because in the last batch
+            # the gradients were modified
+            # insied the optimizer, so in the new batch
+            # there's to carry to zero
+            optimizer.zero_grad() 
+
+            outputs = network(inputs)
+            _, preds = torch.max(outputs, 1)
+            loss = loss_function(outputs, targets)
+            loss.backward() #magias: gradientes calculados automaticamente
+            optimizer.step() #magia2: actualiza las perillas o los parametros
+
+            batch_size = inputs.size()[0]
+            running_loss.update(loss.item()*batch_size,
+                       batch_size)
+            running_acc.update(torch.sum(preds == targets).float(),
+                       batch_size)
+
+        print("Loss: {:.4f} Acc: {:.4f}".format(running_loss(), running_acc()))
+
+
+
 
 
 if __name__ == "__main__":
