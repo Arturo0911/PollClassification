66c0356b44be93a91b8223d062bb2bfc4923d4a8
Payload <anegreiross@outlook.com>
2021-09-25 14:20:42 -0500

    updating process

--- microbian_regression.py
+++ microbian_regression.py
@@ -1,5 +1,6 @@
-"""Growth bacterian prediction"""
+"""Growth bacteria prediction"""
 
+from pprint import pprint
 import pandas as pd
 import torch
 from torch import nn
@@ -13,14 +14,37 @@ from sklearn.model_selection import train_test_split
 DATA_PATH = "datasets/data_regression_set.csv"
 NUM_EPOCHS = 150
 BATCH_SIZE = 64
+LEARNING_RATE = 0.001
 
 
-class BacterianDataset(Dataset):
+def split_dataset(path_dir) -> list:
+    dataset = pd.read_csv(path_dir)
+    features = dataset.drop(["quality_product", "lactobacillus_final_cfu_ml",
+                             "streptococcus_final_cfu_ml",
+                             "lactobacillus_initial_strain_cfu_ml"], axis=1)
+    targets = dataset["streptococcus_final_cfu_ml"]
+    return train_test_split(features, targets, test_size=0.3, shuffle=True, random_state=42)
+
+
+class RunningMetrics:
+    def __init__(self):
+        self.values = 0
+        self.elements = 0
+
+    def update(self, values: float, elements: int):
+        self.values += values
+        self.elements += elements
+
+    def __call__(self):
+        return self.values/float(self.elements)
+
+
+class BacteriaDataset(Dataset):
     def __init__(self, features, targets, transform=None):
         self.features = features
         self.targets = targets
         self.transform = transform
-    
+
     def __len__(self):
         return len(self.targets)
 
@@ -34,56 +58,77 @@ class LinearRegression(nn.Module):
         self.layer1 = nn.Linear(num_features, 12)
         self.layer2 = nn.Linear(12, 24)
         self.layer3 = nn.Linear(24, 12)
-        self.latyer_out = nn.Linear(12, 1)
+        self.layer_out = nn.Linear(12, 1)
         self.relu = nn.ReLU(True)
 
     def forward(self, inputs):
         x = self.relu(self.layer1(inputs))
         x = self.relu(self.layer2(x))
         x = self.relu(self.layer3(x))
-        x = self.latyer_out(x)
+        x = self.layer_out(x)
         return x
 
     def predict(self, test_inputs):
         x = self.relu(self.layer1(test_inputs))
         x = self.relu(self.layer2(x))
         x = self.relu(self.layer3(x))
-        x = self.latyer_out(x)
+        x = self.layer_out(x)
         return x
+
 
 def main():
     # defining a dataset to make split in the features
     # and the targets in the values
-    def split_dataset(path_dir) -> list:
-        dataset = pd.read_csv(path_dir)
-        features = dataset.drop(["quality_product", "lactobacillus_final_cfu_ml", 
-                       "streptococcus_final_cfu_ml", 
-                       "lactobacillus_initial_strain_cfu_ml"], axis=1)
-        targets = dataset["streptococcus_final_cfu_ml"]
-        return train_test_split(features, targets, test_size=0.3, shuffle=True, random_state=42)
-    
+
     X_train, X_test, y_train, y_test = split_dataset(DATA_PATH)
     NUM_FEATURES = len(X_train.columns)
-    print(X_train.columns)
     X_train, X_test = X_train.to_numpy(), X_test.to_numpy()
     y_train, y_test = y_train.to_numpy(), y_test.to_numpy()
 
-    train_dataset = BacterianDataset(torch.from_numpy(X_train).float(), 
+    # spliting dataset in the train and values 
+    train_dataset = BacteriaDataset(torch.from_numpy(X_train).float(),
                                     torch.from_numpy(y_train).float())
 
-    test_dataset = BacterianDataset(torch.from_numpy(X_test).float(), 
-                                    torch.from_numpy(y_test).float())
-
+    test_dataset = BacteriaDataset(torch.from_numpy(X_test).float(),
+                                   torch.from_numpy(y_test).float())
     
+    # instancing dataloader
     data_loader = DataLoader(dataset=train_dataset, batch_size=1)
     test_loader = DataLoader(dataset=test_dataset, batch_size=1)
-    # print(data_loader)
-    re_model = LinearRegression(NUM_FEATURES)
 
-    print("\n\nTRAININ GONNA BE START\n\n")
+    model = LinearRegression(NUM_FEATURES)
 
-    
+    # criterion for the parameters to make the metrics
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
+    # Beginning the train
+    try:
+        for x in range(1, NUM_EPOCHS + 1):
+            print(f"epochs => {x}/{NUM_EPOCHS}")
+            
+            running_loss = RunningMetrics()
+            running_acc = RunningMetrics()
+
+            for inputs, targets in data_loader:
+                optimizer.zero_grad()
+
+                outputs = model(inputs)
+                # print(outputs.item())
+                """_, preds = torch.max(outputs, 1)
+                loss = criterion(outputs, targets)
+                loss.backward()
+                optimizer.step()
+
+                print(preds, outputs)"""
+                # print(outputs.item())
+                # print(targets.unsqueeze(1))
+
+                loss = criterion(outputs, targets.unsqueeze(1))
+                print("\n\n loss value: ", loss)
+                raise StopIteration
+    except Exception as e:
+        print(str(e))
 
 
 if __name__ == "__main__":
--- scratch.py
+++ scratch.py
@@ -1,5 +1,4 @@
 
-
 import os
 from typing import Any
 import torch
@@ -29,30 +28,30 @@ class Net(nn.Module):
         self.conv2 = nn.Conv2d(self.num_channels, self.num_channels * 2, 3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(self.num_channels *2, self.num_channels * 4, 3, stride=1, padding=1)
 
-
         # fully connectted linear
-        self.fc1 = nn.Linear(self.num_channels*4*8*8, self.num_channels*4) # the output of the convolutional layers
-        self.fc2 = nn.Linear(self.num_channels*4, 6)
-    
+        self.fc1 = nn.Linear(self.num_channels * 4 * 8 * 8,
+                             self.num_channels * 4)  # the output of the convolutional layers
+        self.fc2 = nn.Linear(self.num_channels * 4, 6)
+
     def forward(self, x) -> Any:
         """At the start we do
         with an image with 3 channels and 64 x 64 pixels"""
 
         # convolutional layer 1
-        x = self.conv1(x) # num channels x 64 x64
-        x = F.relu(F.max_pool2d(x, 2)) # parameter two say, to divide by 2 => num channels x 32 x 32
+        x = self.conv1(x)  # num channels x 64 x64
+        x = F.relu(F.max_pool2d(x, 2))  # parameter two say, to divide by 2 => num channels x 32 x 32
 
         # convolutional layer 2
-        x = self.conv2(x) # num channels*2 x 32 x32
-        x = F.relu(F.max_pool2d(x, 2)) # dived by 2 => channels *2 16 x 16
+        x = self.conv2(x)  # num channels*2 x 32 x32
+        x = F.relu(F.max_pool2d(x, 2))  # dived by 2 => channels *2 16 x 16
 
         # convolutional 3
-        x = self.conv3(x) # num channels*4  x 16 x 16
-        x = F.relu(F.max_pool2d(x, 2)) # num chanells x 8 x 8
+        x = self.conv3(x)  # num channels*4  x 16 x 16
+        x = F.relu(F.max_pool2d(x, 2))  # num chanells x 8 x 8
 
         # flatten
         # -1 say we gonna do the flatten
-        x = x.view(-1, self.num_channels*4*8*8)
+        x = x.view(-1, self.num_channels * 4 * 8 * 8)
 
         # fully connected
 
@@ -65,31 +64,30 @@ class Net(nn.Module):
         return x
 
 
-# mouting dataset
+# mounting dataset
 class SIGNSDataset(Dataset):
     def __init__(self, base_dir, split="train", transform=None):
         path = os.path.join(base_dir, "{}_signs".format(split))
-        
+
         files = os.listdir(path)
         self.filenames = [os.path.join(path, f) for f in files if f.endswith(".jpg")]
         self.targets = [int(f[0]) for f in files]
         self.transform = transform
-    
+
     def __len__(self):
         return len(self.filenames)
-    
+
     def __getitem__(self, idx):
         image = Image.open(self.filenames[idx])
         if self.transform:
             image = self.transform(image)
-        
+
         return image, self.targets[idx]
 
 
 def main():
-    
-    signs = SIGNSDataset(base_dir="datasets", 
-                         split="train", 
+    signs = SIGNSDataset(base_dir="datasets",
+                         split="train",
                          transform=transforms.ToTensor())
     print(signs[0][0])
 
@@ -98,16 +96,13 @@ class RunningMetric:
     def __init__(self):
         self.S = 0
         self.N = 0
-    
+
     def update(self, val, size):
         self.S += val
         self.N += size
-    
+
     def __call__(self):
-        return self.S/float(self.N)
-
-
-
+        return self.S / float(self.N)
 
 
 # def main():
@@ -151,9 +146,6 @@ class RunningMetric:
 #                        batch_size)
 
 #         print("Loss: {:.4f} Acc: {:.4f}".format(running_loss(), running_acc()))
-
-
-
 
 
 if __name__ == "__main__":
