a0b651564eb63b1c14803ad5834c93eec116f9ed
Arturo0911 <anegreiross@outlook.com>
2021-10-03 15:07:39 -0500

    accuracy ok

--- image-regressor/main.py
+++ image-regressor/main.py
@@ -1,4 +1,4 @@
-# from metrics.metrics import RunningMetrics
+from metrics.metrics import RunningMetrics
 from torch import nn
 from torch.nn import functional as F
 from torch.utils.data import (
@@ -8,8 +8,8 @@ from torch.utils.data import (
 from PIL import Image
 from torchvision import transforms
 import os
-
-
+from torch import optim
+import torch
 
 
 class ImageRegression(nn.Module):
@@ -54,9 +54,9 @@ class ImageDataset(Dataset):
         path = os.path.join(basedir, f"{split}_signs")
         files = os.listdir(path)
 
-        self.filenames = [os.path.join(basedir, file) for file in files if file.endswith(".jpg")]
+        self.filenames = [os.path.join(path, file) for file in files if file.endswith(".jpg")]
 
-        self.target = [int(f[0]) for f in files]
+        self.targets = [int(f[0]) for f in files]
         self.transform = transform
 
     def __len__(self):
@@ -66,16 +66,39 @@ class ImageDataset(Dataset):
         image = Image.open(self.filenames[item])
         if self.transform:
             image = self.transform(image)
-        return image
+        return image, self.targets[item]
 
 
 def main():
 
     trainset = ImageDataset(basedir="datasets", split="train", transform = transforms.ToTensor())
     dataloader = DataLoader(trainset, batch_size=32)
+    net = ImageRegression(32)
+    loss_n = nn.NLLLoss()
+    optimizer = optim.SGD(net.parameters(), lr=1e-3, momentum=0.9)
 
-    for inputs, targets in dataloader:
-        print(inputs, targets)
+    for epoch in range(100):
+        print(f"Epoch {epoch+1}/100")
+
+        running_loss = RunningMetrics()
+        running_acc = RunningMetrics()
+
+        for inputs, targets in dataloader:
+            optimizer.zero_grad()
+            outputs = net(inputs)
+
+            _, preds = torch.max(outputs, 1)
+            loss = loss_n(outputs, targets)
+
+            loss.backward()
+            optimizer.step()
+
+
+            batch_size = inputs.size()[0]
+            running_loss.update(loss.item() * batch_size, batch_size)
+            running_acc.update(torch.sum(preds == targets).float(), batch_size)
+
+        print("Loss: {:.4f} Acc: {:.4f}".format(running_loss(), running_acc()))
 
 
 if __name__ == "__main__":
